5af1475044cc6329553cc4f7a4af971f50a54a86
Adriann Guedes <adrianguedes902@gmail.com>
2024-01-21 13:28:55 -0300

    initial configs

---
+++ src/main/java/com/PicpaySimples/PicpaySimplesApplication.java
@@ -0,0 +1,13 @@
+package com.PicpaySimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaySimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaySimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicpaySimples/PicpaySimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicpaySimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
