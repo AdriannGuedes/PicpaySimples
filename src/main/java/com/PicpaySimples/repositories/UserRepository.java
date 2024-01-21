d1ec606d59b0515b63a34e93bdc6501427742f50
Adriann Guedes <adrianguedes902@gmail.com>
2024-01-21 13:47:24 -0300

    create UserRepository

---
+++ src/main/java/com/PicpaySimples/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.PicpaySimples.repositories;
+
+import com.PicpaySimples.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+
+}
