106b2a54ef79c44c5d2e248b58e58f29bbefc5cd
Adriann Guedes <adrianguedes902@gmail.com>
2024-01-24 21:25:44 -0300

    create create notifications, transaction and users

---
+++ src/main/java/com/PicpaySimples/dto/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.PicpaySimples.dto;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/PicpaySimples/infra/ControllerException.java
@@ -0,0 +1,28 @@
+package com.PicpaySimples.infra;
+
+import com.PicpaySimples.dto.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerException {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exeption){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário ja cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exeption){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
